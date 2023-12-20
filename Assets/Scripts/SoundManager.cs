2576d2bc75f62822a040b8b07bfe6356b0d7dc80
eva0519 <kimyuntae19@gmail.com>
2023-12-21 06:03:02 +0900

    Sound Manager Added

---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,101 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+// 클래스 자체를 직렬화 SerializeField처럼 인스펙터창에 띄운다
+[System.Serializable]
+public class Sound
+{
+    public string name; // 곡의 이름
+    public AudioClip clip; // 곡
+}
+
+public class SoundManager : MonoBehaviour
+{
+    public static SoundManager instance;
+    #region Singleton
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+    #endregion Singleton
+
+    public AudioSource[] audioSourceEffects;
+    public AudioSource audioSourceBgm;
+
+    public string[] playSoundName;
+
+    public Sound[] effectSounds;
+    public Sound[] bgmSounds;
+
+    private void Start()
+    {
+        playSoundName = new string[audioSourceEffects.Length];
+    }
+
+    public void PlaySE(string _name)
+    {
+        for(int i = 0; i < effectSounds.Length; i++)
+        {
+            if(_name == effectSounds[i].name)
+            {
+                for(int j = 0; j < audioSourceEffects.Length; j++)
+                {
+                    if (!audioSourceEffects[j].isPlaying)
+                    {
+                        playSoundName[j] = effectSounds[i].name;
+                        audioSourceEffects[j].clip = effectSounds[i].clip;
+                        audioSourceEffects[j].Play();
+                        return;
+                    }
+                }
+                Debug.Log("모든 가용 AudioSource가 사용중입니다.");
+                return;
+            }
+        }
+        Debug.Log(_name + "사운드가 SoundManager에 등록되지 않았습니다.");
+    }
+
+    public void StopAllSE() 
+    {
+        for (int i = 0; i < audioSourceEffects.Length; i++)
+        {
+            audioSourceEffects[i].Stop();
+        }
+    }
+
+    public void StopSE(string _name)
+    {
+        for (int i = 0; i < audioSourceEffects.Length; i++)
+        {
+            if (playSoundName[i] == _name)
+            {
+                audioSourceEffects[i].Stop();
+                return;
+            }
+        }
+        Debug.Log("재생 중인" + _name + "사운드가 없습니다.");
+    }
+
+    public void PlayBGM(string _name)
+    {
+        for (int i = 0; i < bgmSounds.Length; i++)
+        {
+            if (_name == bgmSounds[i].name)
+            {
+                audioSourceBgm.Stop();
+                audioSourceBgm.clip = bgmSounds[i].clip;
+                audioSourceBgm.Play();
+            }
+        }
+    }
+}
