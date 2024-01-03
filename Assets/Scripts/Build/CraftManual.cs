9930077109a12ccb4843f06f6d3fe6d9f2f8e8c1
eva0519 <kimyuntae19@gmail.com>
2024-01-04 08:06:13 +0900

    craft system extend working

--- Assets/Scripts/Build/CraftManual.cs
+++ Assets/Scripts/Build/CraftManual.cs
@@ -3,11 +3,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEditor.PackageManager.UI;
 using UnityEngine;
+using UnityEngine.UI;
 
 [System.Serializable]
 public class Craft
 {
     public string craftName; //이름
+    public Sprite craftImage; // 이미지
+    public string craftDesc; // 설명
     public GameObject go_Prefab; //실제 설치될 프리팹
     public GameObject go_PreviewPrefab;
 }
@@ -20,7 +23,14 @@ public class CraftManual : MonoBehaviour
     private bool isPreviewActivated = false;
 
     [SerializeField] private GameObject go_BaseUI; // 기본 베이스 UI
+
+    private int tabNumber = 0;
+    private int page = 1;
+    private int selectedSlotNumber;
+    private Craft[] craft_SelectedTab;
+
     [SerializeField] Craft[] craft_fire; // 모닥불용 탭
+    [SerializeField] Craft[] craft_build; // 건축용 탭
 
     private GameObject go_Preview; // 미리보기 프리팹을 담을 변수
     private GameObject go_Prefab; // 실제 생성될 프리팹을 담을 변수
@@ -30,7 +40,58 @@ public class CraftManual : MonoBehaviour
     private RaycastHit hitInfo;
     [SerializeField] private LayerMask layerMask;
     [SerializeField] private float range;
-    
+
+    // 필요한 UI Slot 요소
+    [SerializeField] private GameObject[] go_Slots;
+    [SerializeField] private Image[] image_Slot;
+    [SerializeField] private Text[] text_SlotName;
+    [SerializeField] private Text[] text_SlotDesc;
+
+    private void Start()
+    {
+        tabNumber = 0;
+        page = 1;
+    }
+
+    public void TabSetting(int _tabNumber)
+    {
+        tabNumber = _tabNumber;
+        // 다른 페이지에서 동일 탭 클릭시 1페이지로 이동
+        page = 1;
+
+        switch (tabNumber)
+        {
+            case 0:
+                TabSlotSetting(craft_fire);
+                break;
+            case 1:
+                TabSlotSetting(craft_build);
+                break;
+        }
+    }
+
+    private void TabSlotSetting(Craft[] _craft_tab)
+    {
+        craft_SelectedTab = _craft_tab;
+
+        // 페이저 구현부
+        // 0, 4, 8... 4의 배수; 해당 page에 몇번 인덱스의 build 아이템을 보여줄지 계산
+        // 0 1 2 3 / 4 5 6 7 / 8 9 10 11
+        int startSlotNumber = (page - 1) * go_Slots.Length;
+
+        for (int i = startSlotNumber; i < craft_SelectedTab.Length; i++)
+        {
+            if (i == page * go_Slots.Length)
+                break;
+
+            go_Slots[i - startSlotNumber].SetActive(true);
+
+            image_Slot[i - startSlotNumber].sprite = craft_SelectedTab[i].craftImage;
+            text_SlotName[i - startSlotNumber].text = craft_SelectedTab[i].craftName;
+            text_SlotDesc[i - startSlotNumber].text = craft_SelectedTab[i].craftDesc;
+        }
+    }
+
     public void SlotClick(int _slotNumber)
     {
         go_Preview = Instantiate(craft_fire[_slotNumber].go_PreviewPrefab, tf_Player.position + tf_Player.forward, Quaternion.identity);
